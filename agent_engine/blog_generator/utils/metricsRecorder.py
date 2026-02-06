c150b026e6d9e9da0bb89d023575aaeb1970e3d0
Mustafa <mustafabutt69@gmail.com>
2026-02-07 00:38:30 +0500

    env changed to prod in metrix

--- agent_engine/blog_generator/utils/metricsRecorder.py
+++ agent_engine/blog_generator/utils/metricsRecorder.py
@@ -50,9 +50,9 @@ class MetricsRecorder:
         if run_env is None:
             # Check common environment variables
             if os.getenv("CI") or os.getenv("GITHUB_ACTIONS"):
-                self.run_env = "DEV"
+                self.run_env = "PROD"
             elif os.getenv("ENVIRONMENT") == "production":
-                self.run_env = "DEV"
+                self.run_env = "PROD"
             else:
                 self.run_env = "DEV"
         else:
